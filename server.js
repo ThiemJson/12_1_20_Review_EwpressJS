f01b25d9dbc0d305afef79f020e567a2d5464193
thiemjson <thiemjson@gmail.com>
2020-01-12 17:07:11 +0700

    Tèn tèn

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const bodyParser = require('body-parser');
 
 app.set('views', './views'); // Thư mục views nằm cùng cấp với file app.js
 app.set('view engine', 'pug'); // Sử dụng pug làm view engine
-// Một cái Database đơn giản
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+// // Một cái Database đơn giản
 users =[
     {id: 1, name: "CaoThiem"},
     {id: 2, name: "MinhChou"},
@@ -21,8 +24,18 @@ app.listen(port,(req,res)=>{
 
 
 //app.get 
-app.get(`/views/users`,(request,response)=>{
+app.get(`/`,(request,response)=>{
     response.render(`users/user.pug`,{
         users:users
+    })
+})
+
+app.get(`/users/search`,(request,response)=>{
+    let name = request.query.name.toLowerCase();
+    let matchedUsers = users.filter((user)=>{
+        return user.name.toLowerCase().indexOf(name) !== -1;
+    })
+    response.render(`users/user.pug`,{
+        users : matchedUsers
     })
 })
