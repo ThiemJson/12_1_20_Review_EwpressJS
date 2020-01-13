85eaed9b26a89915a74234e0f8bf266a3ccfb8aa
thiemjson <thiemjson@gmail.com>
2020-01-13 23:39:45 +0700

    reallylyylyl

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -5,3 +5,20 @@ module.exports.server = (request,response)=>{
         users:db.get("users").value()
     })
 }
+module.exports.search = (request,response)=>{
+    let name = request.query.name.toLowerCase();
+    let matchedUsers = db.get("users").value().filter((user)=>{
+        return user.name.toLowerCase().indexOf(name) !== -1;
+    })
+    response.render(`users/user.pug`,{
+        users : matchedUsers
+    })
+}
+module.exports.create = (request,response)=>{
+    response.render('users/createUser.pug');
+}
+
+module.exports.createPost = (request,response)=>{
+    db.get('users').push(request.body).write()
+    response.redirect("/users");
+}
--- routes/user.rount.js
+++ routes/user.rount.js
@@ -1,25 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const db = require("../db")
 const controllers = require("../controllers/user.controllers")
 
-router.get(`/`,controllers.server);
-router.get(`/search`,(request,response)=>{
-    let name = request.query.name.toLowerCase();
-    let matchedUsers = users.filter((user)=>{
-        return user.name.toLowerCase().indexOf(name) !== -1;
-    })
-    response.render(`users/user.pug`,{
-        users : matchedUsers
-    })
-})
-router.get(`/create`,(request,response)=>{
-    response.render('users/createUser.pug');
-})
-router.post(`/create`,(request,response)=>{
-    db.get('users').push(request.body).write()
-    response.redirect("/users");
-})
+router.get(`/`, controllers.server);
+router.get(`/search`, controllers.search)
+router.get(`/create`, controllers.create)
+router.post(`/create`, controllers.createPost)
 
 
 module.exports = router;
